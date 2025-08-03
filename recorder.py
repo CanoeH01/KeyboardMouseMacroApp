5419b56d7e664eaa3872c0bfe6763d19fcf07489
Canoe <conorhanrahan01@gmail.com>
2025-08-03 11:12:32 +0100

    Added functionality for reading keyboard inputs

--- recorder.py
+++ recorder.py
@@ -1,20 +1,29 @@
 import time
 
-from pynput import mouse
+from pynput import mouse, keyboard
+
 
 class InputRecorder:
     def __init__(self):
         self.events = []
         self.start_time = None
 
-    def start(self):
-        listener = mouse.Listener(
+        self.mouseListener = mouse.Listener(
             on_move=self.on_move,
             on_click=self.on_click,
             on_scroll=self.on_scroll)
-        listener.start()
+
+        self.keyboardListener = keyboard.Listener(
+            on_press=self.on_press,
+            on_release=self.on_release)
+
+    def start(self):
+
         self.start_time = time.time()
-        listener.join()
+        self.mouseListener.start()
+        self.keyboardListener.start()
+        self.mouseListener.join()
+        self.keyboardListener.join()
 
     def getTime(self):
         return time.strftime("%H:%M:%S", time.localtime())
@@ -27,3 +36,12 @@ class InputRecorder:
 
     def on_scroll(self, x, y, dx, dy):
         print('Scrolled', 'down' if dy < 0 else 'up',(x, y), self.getTime())
+
+    def on_press(self, key):
+        print(key, 'pressed')
+        if key == keyboard.Key.esc:
+            self.keyboardListener.stop()
+            self.mouseListener.stop()
+
+    def on_release(self, key):
+        print(key, 'released')
