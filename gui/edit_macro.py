867e780dc4eeaea82ff0175807a06676586c9fa3
Canoe <conorhanrahan01@gmail.com>
2025-08-14 12:19:55 +0100

    Hooked macro data up to QTreeWidget object Can view macro information but not edit yet

--- gui/edit_macro.py
+++ gui/edit_macro.py
@@ -1,8 +1,47 @@
-from PySide6.QtWidgets import QDialog
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QDialog, QTreeWidgetItem, QHeaderView
+
 from gui.ui_edit_macro import Ui_formEdit
 
 class EditMacroForm(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, macro, parent=None):
         super().__init__()
         self.ui = Ui_formEdit()
         self.ui.setupUi(self)
+
+        self.selected_macro = macro
+
+        self.ui.lblMacroName.setText(self.selected_macro['name'])
+        self.populateMacroTree()
+
+    def populateMacroTree(self):
+        self.ui.treeMacroData.clear()
+        self.ui.treeMacroData.header().setSectionResizeMode(QHeaderView.ResizeMode.ResizeToContents)
+        for step in self.selected_macro['steps']:
+            self.addStepToTree(self.ui.treeMacroData, step)
+
+    def addStepToTree(self, parent, step):
+        item = QTreeWidgetItem(parent)
+        item.setText(0, str(step["id"]))
+        item.setText(1, step["type"].replace("_", " "))
+
+        match step["type"]:
+            case "mouse_move" | "mouse_scroll":
+                pos_string = "x: " + str(step["pos"][0]) + "\ny: " + str(step["pos"][1])
+                item.setText(2, pos_string)
+            case "mouse_click":
+                item.setText(2, step["button"])
+            case "key_press" | "key_release":
+                if "Key." in step["key"]:
+                    item.setText(2, step["key"][4:].replace("_", " + "))
+                else:
+                    item.setText(2, step["key"].strip("\'"))
+            case "if":
+                item.setText(2, step["condition"])
+
+        item.setData(0, Qt.UserRole, step)
+        item.setTextAlignment(2, Qt.AlignmentFlag.AlignRight)
+
+        if "steps" in step:
+            for child_step in step["steps"]:
+                self.addStepToTree(item, child_step)
--- gui/ui_edit_macro.py
+++ gui/ui_edit_macro.py
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QSizePolicy,
-    QTreeWidget, QTreeWidgetItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QLabel,
+    QSizePolicy, QTreeWidget, QTreeWidgetItem, QWidget)
 
 class Ui_formEdit(object):
     def setupUi(self, formEdit):
@@ -27,11 +27,30 @@ class Ui_formEdit(object):
         self.gridLayout.setObjectName(u"gridLayout")
         self.treeMacroData = QTreeWidget(formEdit)
         __qtreewidgetitem = QTreeWidgetItem()
-        __qtreewidgetitem.setText(0, u"1");
+        __qtreewidgetitem.setTextAlignment(2, Qt.AlignCenter);
+        __qtreewidgetitem.setTextAlignment(1, Qt.AlignCenter);
+        __qtreewidgetitem.setTextAlignment(0, Qt.AlignCenter);
         self.treeMacroData.setHeaderItem(__qtreewidgetitem)
         self.treeMacroData.setObjectName(u"treeMacroData")
+        self.treeMacroData.setStyleSheet(u"    QTreeWidget::item {\n"
+"        padding-top: 3px;\n"
+"		 padding-bottom: 3px;    \n"
+"    }\n"
+"")
+        self.treeMacroData.setAnimated(True)
+        self.treeMacroData.setColumnCount(3)
+        self.treeMacroData.header().setVisible(True)
 
-        self.gridLayout.addWidget(self.treeMacroData, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.treeMacroData, 1, 0, 1, 1)
+
+        self.lblMacroName = QLabel(formEdit)
+        self.lblMacroName.setObjectName(u"lblMacroName")
+        font = QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        self.lblMacroName.setFont(font)
+
+        self.gridLayout.addWidget(self.lblMacroName, 0, 0, 1, 1)
 
 
         self.retranslateUi(formEdit)
@@ -41,5 +60,10 @@ class Ui_formEdit(object):
 
     def retranslateUi(self, formEdit):
         formEdit.setWindowTitle(QCoreApplication.translate("formEdit", u"Edit Macro", None))
+        ___qtreewidgetitem = self.treeMacroData.headerItem()
+        ___qtreewidgetitem.setText(2, QCoreApplication.translate("formEdit", u"Details", None));
+        ___qtreewidgetitem.setText(1, QCoreApplication.translate("formEdit", u"Action", None));
+        ___qtreewidgetitem.setText(0, QCoreApplication.translate("formEdit", u"Step", None));
+        self.lblMacroName.setText(QCoreApplication.translate("formEdit", u"[MacroName]", None))
     # retranslateUi
 
